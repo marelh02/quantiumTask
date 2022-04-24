9b49d7da1d27701d739332efa09b486bcae905c2
marelh <e.marouane02@gmail.com>
2022-04-24 11:39:49 +0000

    Fourth task completed!

--- dashApp.py
+++ dashApp.py
@@ -1,4 +1,5 @@
-from dash import Dash, html, dcc
+from secrets import choice
+from dash import Dash, html, dcc, Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 
@@ -10,12 +11,10 @@ sDF=mainDF.loc[mainDF["Region"]=="south"].filter(["Date","Sales"])
 eDF=mainDF.loc[mainDF["Region"]=="east"].filter(["Date","Sales"])
 wDF=mainDF.loc[mainDF["Region"]=="west"].filter(["Date","Sales"])
 
+lizt=[nDF,sDF,eDF,wDF]
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=nDF["Date"], y=nDF["Sales"], name="North", mode="lines"))
-fig.add_trace(go.Scatter(x=sDF["Date"], y=sDF["Sales"], name="South", mode="lines"))
-fig.add_trace(go.Scatter(x=eDF["Date"], y=eDF["Sales"], name="East", mode="lines"))
-fig.add_trace(go.Scatter(x=wDF["Date"], y=wDF["Sales"], name="West", mode="lines"))
+
 
 fig.update_layout(
     title="Were sales higher before or after the Pink Morsel price increase on the 15th of January, 2021?",
@@ -27,8 +26,35 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='salesGraph',
         figure=fig
-    )
+    ),
+    html.Div(children=dcc.RadioItems(['North', 'South','East','West','All'], 'All', inline=True,id='choices'),
+    id="radio")
 ])
 
+@app.callback(
+    Output('salesGraph', 'figure'),
+    Input('choices', 'value'))
+def set_display_children(choice):
+    if choice=="North":
+        fig= go.Figure()
+        fig.add_trace(go.Scatter(x=nDF["Date"], y=nDF["Sales"], name="North", mode="lines"))
+    elif choice=="South":
+        fig= go.Figure()
+        fig.add_trace(go.Scatter(x=sDF["Date"], y=sDF["Sales"], name="South", mode="lines"))
+    elif choice=="East":
+        fig= go.Figure()
+        fig.add_trace(go.Scatter(x=eDF["Date"], y=eDF["Sales"], name="East", mode="lines"))
+    elif choice=="West":
+        fig= go.Figure()
+        fig.add_trace(go.Scatter(x=wDF["Date"], y=wDF["Sales"], name="West", mode="lines"))
+    else:
+        fig= go.Figure()
+        fig.add_trace(go.Scatter(x=nDF["Date"], y=nDF["Sales"], name="North", mode="lines"))
+        fig.add_trace(go.Scatter(x=sDF["Date"], y=sDF["Sales"], name="South", mode="lines"))
+        fig.add_trace(go.Scatter(x=eDF["Date"], y=eDF["Sales"], name="East", mode="lines"))
+        fig.add_trace(go.Scatter(x=wDF["Date"], y=wDF["Sales"], name="West", mode="lines"))
+    return fig
+
+        
 if __name__ == '__main__':
     app.run_server(debug=True)
