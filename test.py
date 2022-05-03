b06972b4dbc74f2d784037257cfc3eb04de2569d
marelh <e.marouane02@gmail.com>
2022-05-03 00:05:06 +0000

    "dash testing"

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-import pandas as pd
+import dash
+from dash import html
 
 
-mainDF = pd.read_csv("data\processedData.csv")
-nDF=mainDF.loc[mainDF["Region"]=="north"].filter(["Date","Sales"])
-print(nDF)
+def test_bsly001_falsy_child(dash_duo):
+    
+    app = dash.Dash(__name__)
+    app.layout = html.Div(id="nully-wrapper", children=0)
+    dash_duo.start_server(app)
+    dash_duo.wait_for_text_to_equal("#nully-wrapper", "0", timeout=4)
+    assert dash_duo.find_element("h1")
+    assert dash_duo.find_element("div#radio")
+    assert dash_duo.find_element("div#salesGraph")
